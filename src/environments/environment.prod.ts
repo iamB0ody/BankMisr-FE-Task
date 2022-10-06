747d3041f5d18f8d3e07680930a9cf9400a43859
iamB0ody <abdelrahman.ui@gmail.com>
2022-10-06 19:21:20 +0200

    🔧  Configure enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  // api: "https://data.fixer.io/api/",
+  api: 'https://api.apilayer.com/fixer/',
+  apiKey: '9mFtr2HtZdrWTFPGGSyB81mk6gHGekj8',
 };
